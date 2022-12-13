92614bd26a569cc8ddbe8282c40f9fa460c0223f
Jonathan Allsop <jonathanallsop2@gmail.com>
2022-12-13 22:01:25 +0000

    Adds the combine_description_strings function.

--- tabular_data.py
+++ tabular_data.py
@@ -1,29 +1,59 @@
 import pandas as pd
 import numpy as np
+from ast import literal_eval
 
-df = pd.read_csv("tabular_data/listing.csv")
+df = pd.read_csv("tabular_data/fixed_listing.csv")
 
 def remove_rows_with_missing_ratings(df):
 
     rating_headers = ["Cleanliness_rating","Accuracy_rating","Communication_rating","Location_rating","Check-in_rating","Value_rating"]
     
     df = df.dropna(axis=0,how='any',subset=rating_headers)
+
+    df = df.reset_index(drop=True)
     
     return df
     
+        
+def combine_description_strings(df):
+
+    df = df.dropna(axis=0,how='any',subset=["Description"])
+
+    df = df.reset_index(drop=True)
+
+    new_descriptions = []
     
-    """ size = df.shape[0]
+    for i in range(df.shape[0]):
 
-    for i in range(size):
-        rating_headers = ["Cleanliness_rating","Accuracy_rating","Communication_rating","Location_rating","Check-in_rating","Value_rating"]
-        rating_missing = False
-        for j in range(6):
-            if np.isnan(df[rating_headers[j]][i]) == True:
-                rating_missing = True
-        
-        if rating_missing == True:
-            df.drop() """
-        
-df = remove_rows_with_missing_ratings(df)
+        try:
+            description_list = literal_eval(df['Description'][i])
+        except SyntaxError:
+            problem_id = df['ID'][i]
+            print(f'\n\nThere is an issue with the description of property {problem_id}\n\n')
+            df = df.drop(i)
+            continue
 
-print(df)
+        no_whitespace = [string for string in description_list if string.strip()]
+        new_description = " ".join(no_whitespace[1:])
+
+
+        new_descriptions.append(new_description)
+
+    df = df.reset_index(drop=True)
+
+    new_series = pd.Series(new_descriptions)
+    df['Description'] = new_series
+
+    return df
+    
+def clean_tabular_data(df):
+
+    df = remove_rows_with_missing_ratings(df)    
+
+    df = combine_description_strings(df)
+
+    return df
+
+df = clean_tabular_data(df)
+
+print(df['Description'][2])
