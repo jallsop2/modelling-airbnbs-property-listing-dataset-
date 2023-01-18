403a12640c5ab17c5883c1526043009051f53f31
Jonathan Allsop <jonathanallsop2@gmail.com>
2023-01-18 13:08:14 +0000

    First classification model

--- classification_modelling.py
+++ classification_modelling.py
@@ -3,6 +3,7 @@ from tabular_data import load_airbnb
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LogisticRegression
 
 import numpy as np
 
@@ -19,6 +20,18 @@ if __name__ == "__main__":
     scaler.fit(X)
     X = scaler.transform(X)
 
-    print(X.shape,len(y))
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    model = LogisticRegression()
+
+    model.fit(X_train,y_train)
+
+    print(model.score(X_test,y_test))
+
+    test_predictions = model.predict(X_test)
+
+
+
 
     
