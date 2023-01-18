791d6c23c09069a929059ab967e804a8894c271a
Jonathan Allsop <jonathanallsop2@gmail.com>
2023-01-18 12:46:58 +0000

    Adds find_best_model funnction

--- modelling.py
+++ modelling.py
@@ -10,6 +10,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
 
 import numpy as np
+import os
+import json
+import joblib
 
 
 def evaluate_regression_models(data):
@@ -21,26 +24,26 @@ def evaluate_regression_models(data):
     
     models = []
 
-    """ models.append(["linear_regression", SGDRegressor(), {
+    models.append(["linear_regression", SGDRegressor(), {
         "alpha" : [0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.01, 0.1, 0.5],
         "max_iter" : [100, 500, 1000, 5000, 10000, 100000],
         "eta0" : [0.001, 0.005, 0.01, 0.05, 0.1, 0.5],
         "random_state" : [1]
-    }]) """
+    }])
 
-    """ models.append(["regression_tree", DecisionTreeRegressor(), {
+    models.append(["regression_tree", DecisionTreeRegressor(), {
         "max_depth" : [5, 10, 100, 1000, None],
         "min_samples_split" : [1, 2, 5, 10],
         "min_samples_leaf" : [1, 2, 5, 10, 15, 20, 25],
         "random_state" : [1]
-    } ]) """
+    } ])
 
-    """ models.append(["random_forest", RandomForestRegressor(), {
+    models.append(["random_forest", RandomForestRegressor(), {
         "n_estimators" : [100, 500],
         "max_depth" : [10, 100, None],
         "min_samples_leaf" : [1, 5, 10, 15, 20],
         "random_state" : [1]
-    } ])  """
+    } ]) 
 
     models.append(["gradient_boosting", GradientBoostingRegressor(), {
         "subsample" : [0.25, 0.5, 0.66, 0.75],
@@ -56,7 +59,7 @@ def evaluate_regression_models(data):
 
         best_model , best_params, best_score = tune_regression_model_hyperparameters(models[i][1], X_train, y_train, models[i][2])
 
-        print(f"\n\n {models[i][0]} \n")
+        print(f"\n {models[i][0]} \n")
         print(best_params)
         #print(best_model.coef_)
 
@@ -71,7 +74,7 @@ def evaluate_regression_models(data):
         test_r2_score = r2_score(y_test,y_test_pred)
 
         print(f"Training set: RMSE = {training_RMSE}, r2 score = {training_r2_score}")
-        print(f"Test set: RMSE = {test_RMSE}, r2 score = {test_r2_score}")
+        print(f"Test set: RMSE = {test_RMSE}, r2 score = {test_r2_score}\n\n")
 
         metrics = {
             "Training RMSE" : training_RMSE,
@@ -85,7 +88,25 @@ def evaluate_regression_models(data):
         save_model(best_model, best_params, metrics, path)
 
 
+def find_best_model():
+    
+    dirs = os.listdir("models/regression")
+    best_RMSE = np.infty
 
+    for dir in dirs:
+
+        metrics_file = open(f"models/regression/{dir}/metrics.json")
+        metrics = json.loads(metrics_file.read())
+        RMSE = metrics["Test RMSE"]
+        print(RMSE)
+        
+        if RMSE < best_RMSE:
+            hyperparameter_file = open(f"models/regression/{dir}/hyperparameters.json")
+            hyperparameters = json.loads(hyperparameter_file.read())
+            best_model_info = [dir, joblib.load(f"models/regression/{dir}/model.joblib"), hyperparameters, metrics]
+            best_RMSE = RMSE
+
+    return best_model_info
 
 
 if __name__ == "__main__":
@@ -100,4 +121,8 @@ if __name__ == "__main__":
     X = np.concatenate((data[:,:3], data[:,4:]),1)
     #X = data[:,:3]
     #X = np.concatenate((data[:,:3], data[:,10:]),1)
+
     evaluate_regression_models([X,y])
+
+
+    print(find_best_model())
